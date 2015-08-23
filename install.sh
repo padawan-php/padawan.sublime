145688b81ca8d2536c0335f29f27732cc470875c
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2015-08-23 12:03:54 +0300

    added submodules init to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
+git submodule update --init --recursive
 php -r "readfile('https://getcomposer.org/installer');" | php
 cd $DIR/padawan.php
 $DIR/composer.phar install
